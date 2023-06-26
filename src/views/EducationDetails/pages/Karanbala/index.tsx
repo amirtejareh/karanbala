6888d2b4758463554d09ec40e415d85574dfe6de
a.tejareh <a.tejareh@dibaware.local>
2023-06-26 16:53:14 +0330

    feat: add education details pages

---
+++ src/views/EducationDetails/pages/Karanbala/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { useTheme } from "@mui/styles";
+import { ThemeOptions } from "@mui/system";
+import { KaranbalaLogoTextSvg } from "../../../../assets";
+
+const Karanbala = () => {
+    const theme: ThemeOptions = useTheme();
+
+    return (
+        <>
+            <Box
+                margin={"0.75rem 3.25rem 0 3.25rem"}
+                paddingBottom={"7.5rem"}
+                display={"flex"}
+                justifyContent={"flex-end"}
+                gap={"2rem"}
+            >
+                <KaranbalaLogoTextSvg />
+            </Box>
+            <Box margin={"4rem 5.2rem 8rem  5.2rem"}>
+                <Typography fontSize={"3.6rem"} variant="subtitle1">
+                    کران بالا
+                </Typography>
+            </Box>
+        </>
+    );
+};
+
+export default Karanbala;
