d75a21eeaf2fe2900fbf4d2b6347db79f0bb390f
a.tejare <qwer1234>
2024-02-10 10:34:40 +0330

    @ApiConsumes("multipart/form-data")

--- src/services/services/AnswersheetManagementService.ts
+++ src/services/services/AnswersheetManagementService.ts
@@ -2,89 +2,95 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-import type { CreateAnswersheetManagementDto } from "../models/CreateAnswersheetManagementDto";
-import type { UpdateAnswersheetManagementDto } from "../models/UpdateAnswersheetManagementDto";
+import type { CreateAnswersheetManagementDto } from '../models/CreateAnswersheetManagementDto';
+import type { UpdateAnswersheetManagementDto } from '../models/UpdateAnswersheetManagementDto';
 
-import type { CancelablePromise } from "../core/CancelablePromise";
-import { OpenAPI } from "../core/OpenAPI";
-import { request as __request } from "../core/request";
+import type { CancelablePromise } from '../core/CancelablePromise';
+import { OpenAPI } from '../core/OpenAPI';
+import { request as __request } from '../core/request';
 
 export class AnswersheetManagementService {
+
     /**
-     * @param formData
-     * @returns any
+     * @param formData 
+     * @returns any 
      * @throws ApiError
      */
     public static answersheetManagementControllerCreate(
-        formData: CreateAnswersheetManagementDto,
-    ): CancelablePromise<any> {
+formData: CreateAnswersheetManagementDto,
+): CancelablePromise<any> {
         return __request(OpenAPI, {
-            method: "POST",
-            url: "/answersheet-management",
+            method: 'POST',
+            url: '/answersheet-management',
             formData: formData,
-            mediaType: "multipart/form-data",
+            mediaType: 'multipart/form-data',
         });
     }
 
     /**
-     * @returns any
+     * @returns any 
      * @throws ApiError
      */
     public static answersheetManagementControllerFindAll(): CancelablePromise<any> {
         return __request(OpenAPI, {
-            method: "GET",
-            url: "/answersheet-management",
+            method: 'GET',
+            url: '/answersheet-management',
         });
     }
 
     /**
-     * @param id
-     * @returns any
+     * @param id 
+     * @returns any 
      * @throws ApiError
      */
-    public static answersheetManagementControllerFindOne(id: string): CancelablePromise<any> {
+    public static answersheetManagementControllerFindOne(
+id: string,
+): CancelablePromise<any> {
         return __request(OpenAPI, {
-            method: "GET",
-            url: "/answersheet-management/{id}",
+            method: 'GET',
+            url: '/answersheet-management/{id}',
             path: {
-                id: id,
+                'id': id,
             },
         });
     }
 
     /**
-     * @param id
-     * @param formData
-     * @returns any
+     * @param id 
+     * @param formData 
+     * @returns any 
      * @throws ApiError
      */
     public static answersheetManagementControllerUpdate(
-        id: string,
-        formData: UpdateAnswersheetManagementDto,
-    ): CancelablePromise<any> {
+id: string,
+formData: UpdateAnswersheetManagementDto,
+): CancelablePromise<any> {
         return __request(OpenAPI, {
-            method: "PATCH",
-            url: "/answersheet-management/{id}",
+            method: 'PATCH',
+            url: '/answersheet-management/{id}',
             path: {
-                id: id,
+                'id': id,
             },
             formData: formData,
-            mediaType: "multipart/form-data",
+            mediaType: 'multipart/form-data',
         });
     }
 
     /**
-     * @param id
-     * @returns any
+     * @param id 
+     * @returns any 
      * @throws ApiError
      */
-    public static answersheetManagementControllerRemove(id: string): CancelablePromise<any> {
+    public static answersheetManagementControllerRemove(
+id: string,
+): CancelablePromise<any> {
         return __request(OpenAPI, {
-            method: "DELETE",
-            url: "/answersheet-management/{id}",
+            method: 'DELETE',
+            url: '/answersheet-management/{id}',
             path: {
-                id: id,
+                'id': id,
             },
         });
     }
+
 }
