368e9952dbcdf948fa4709e44d1fbc6d48af2f45
a.tejare <qwer1234>
2024-02-27 14:58:05 +0330

    fix: fix some issues

--- src/services/core/OpenAPI.ts
+++ src/services/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-  BASE: "https://api-karanbala.iran.liara.run",
+  BASE: "http://localhost:3000",
   VERSION: "0.1",
   WITH_CREDENTIALS: false,
   CREDENTIALS: "include",
