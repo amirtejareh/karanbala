d06f1bd85b37c96c913bded0c1d79fa8ff16477f
Amir Tejareh <amirtejareh@gmail.com>
2024-04-19 23:26:40 +0330

    fix: fix some issues

--- src/views/EducationDetails/index.tsx
+++ src/views/EducationDetails/index.tsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import {
-    AttachSvg,
-    BookSavedSvg,
-    ExamSvg,
-    ExampleSvg,
-    KaranbalaExamSvg,
-    KaranbalaLogoTextSvg,
-    PointAndTestSvg,
-    PracticeSvg,
-    QuestionsSvg,
-    QuizSvg,
-    TextBookSvg,
+  AttachSvg,
+  BookSavedSvg,
+  ExamSvg,
+  ExampleSvg,
+  KaranbalaExamSvg,
+  KaranbalaLogoTextSvg,
+  PointAndTestSvg,
+  PracticeSvg,
+  QuestionsSvg,
+  QuizSvg,
+  TextBookSvg,
 } from "../../assets";
 import { SelectKit } from "../../components/kit/Select";
 import DrSamiee from "../../assets/images/drsamiee.png";
@@ -25,189 +25,190 @@ import useGetBooksBasedOnGradeLevels from "../../hooks/book/useGetBooksBasedOnGr
 import EducationDetailStore from "../../stores/educationDetailStore";
 
 const useStyles = makeStyles((theme: ThemeOptions) => ({
-    educationDetailsBox: {
-        "& > button": {
-            flexBasis: "22%",
-            margin: "0 auto",
-            flexGrow: "1",
-        },
+  educationDetailsBox: {
+    "& > button": {
+      flexBasis: "22%",
+      margin: "0 auto",
+      flexGrow: "1",
     },
+  },
 }));
 
 const MajorRequirements = () => {
-    const theme: ThemeOptions = useTheme();
-    const classes = useStyles();
-    const navigate = useNavigate();
-    const { setBook, book } = EducationDetailStore((state) => state);
+  const theme: ThemeOptions = useTheme();
+  const classes = useStyles();
+  const navigate = useNavigate();
+  const { setBook, book } = EducationDetailStore((state) => state);
 
-    useEffect(() => {
-        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-    }, []);
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, []);
 
-    const text = [
-        "معرفی کتاب",
-        "درس نامه",
-        "سوالات تشریحی",
-        "نکته و تست",
-        "ضمائم",
-        "آزمون انتخابی",
-        "کران بالا",
-        "نمونه سوالات امتحانی",
-        "تست‌های جامع",
-        "تمارین داخل کتاب",
-    ];
-    const logo = [
-        <BookSavedSvg />,
-        <TextBookSvg />,
-        <QuestionsSvg />,
-        <PointAndTestSvg />,
-        <AttachSvg />,
-        <QuizSvg />,
-        <KaranbalaExamSvg />,
-        <ExampleSvg />,
-        <ExamSvg />,
-        <PracticeSvg />,
-    ];
+  const text = [
+    "معرفی کتاب",
+    "درس نامه",
+    "سوالات تشریحی",
+    "نکته و تست",
+    "ضمائم",
+    "آزمون انتخابی",
+    "کران بالا",
+    "نمونه سوالات امتحانی",
+    "تست‌های جامع",
+    "تمارین داخل کتاب",
+  ];
+  const logo = [
+    <BookSavedSvg />,
+    <TextBookSvg />,
+    <QuestionsSvg />,
+    <PointAndTestSvg />,
+    <AttachSvg />,
+    <QuizSvg />,
+    <KaranbalaExamSvg />,
+    <ExampleSvg />,
+    <ExamSvg />,
+    <PracticeSvg />,
+  ];
 
-    const [selectValue, setSelectValue] = useState();
+  const [selectValue, setSelectValue] = useState();
 
-    const path = [
-        "introduction-book",
-        "lessons",
-        "questions",
-        "point-and-test",
-        "attach",
-        "quiz",
-        "karanbala",
-        "example",
-        "exam",
-        "practice",
-    ];
+  const path = [
+    "introduction-book",
+    "lessons",
+    "questions",
+    "point-and-test",
+    "attach",
+    "quiz",
+    "karanbala",
+    "example",
+    "exam",
+    "practice",
+  ];
 
-    const getBooksBasedOnGradeLevels = useGetBooksBasedOnGradeLevels([
-        localStorage.getItem("gradeLevel"),
-    ]);
+  const getBooksBasedOnGradeLevels = useGetBooksBasedOnGradeLevels([
+    localStorage.getItem("gradeLevel"),
+  ]);
 
-    const [books, setBooks] = useState<{ value: string; title: string }[]>();
+  const [books, setBooks] = useState<{ value: string; title: string }[]>();
 
-    const [disable, setDisable] = useState<boolean>(true);
+  const [disable, setDisable] = useState<boolean>(true);
 
-    useEffect(() => {
-        if (!getBooksBasedOnGradeLevels.isLoading && getBooksBasedOnGradeLevels.data) {
-            setBooks(
-                getBooksBasedOnGradeLevels?.data?.map((elements) => ({
-                    value: elements._id,
-                    title: elements.title,
-                })),
-            );
-        }
-    }, [getBooksBasedOnGradeLevels.data]);
+  useEffect(() => {
+    if (!getBooksBasedOnGradeLevels.isLoading && getBooksBasedOnGradeLevels.data) {
+      setBooks(
+        getBooksBasedOnGradeLevels?.data?.map((elements) => ({
+          value: elements._id,
+          title: elements.title,
+        })),
+      );
+    }
+  }, [getBooksBasedOnGradeLevels.data]);
 
-    useEffect(() => {
-        if (selectValue || book) {
-            setDisable(false);
-        }
-    }, [selectValue, book]);
+  useEffect(() => {
+    if (selectValue || book) {
+      setDisable(false);
+    }
+  }, [selectValue, book]);
 
-    return (
-        <Box margin={"0.75rem 3.25rem 6rem 3.25rem"} paddingBottom={"7.5rem"}>
-            <Box display={"flex"} justifyContent={"flex-end"} gap={"0.8rem"}>
-                <ButtonKit sx={{ padding: 0, minWidth: 0 }} onClick={() => navigate("/")}>
-                    {" "}
-                    <KaranbalaLogoTextSvg />
-                </ButtonKit>
-            </Box>
-            <Box>
-                <Box margin={"3.19rem 0 7.5rem 0"}>
-                    <SelectKit
-                        options={books}
-                        label={"انتخاب کتاب"}
-                        value={book}
-                        onChange={({ target: { value } }) => {
-                            const newValue: any = value as any;
+  useEffect(() => {
+    return () => {
+      setBook(null);
+    };
+  }, []);
 
-                            setBook(newValue);
-                            setSelectValue(newValue);
-                        }}
-                    ></SelectKit>
-                </Box>
+  return (
+    <Box margin={"0.75rem 3.25rem 6rem 3.25rem"} paddingBottom={"7.5rem"}>
+      <Box display={"flex"} justifyContent={"flex-end"} gap={"0.8rem"}>
+        <ButtonKit sx={{ padding: 0, minWidth: 0 }} onClick={() => navigate("/")}>
+          {" "}
+          <KaranbalaLogoTextSvg />
+        </ButtonKit>
+      </Box>
+      <Box>
+        <Box margin={"3.19rem 0 7.5rem 0"}>
+          <SelectKit
+            options={books}
+            label={"انتخاب کتاب"}
+            value={book}
+            onChange={({ target: { value } }) => {
+              const newValue: any = value as any;
 
-                <Box
-                    className={`drSamieeBox`}
-                    display={"flex"}
-                    justifyContent={"space-around"}
-                    alignItems={"center"}
-                >
-                    <Box flexBasis={"40%"}>
-                        <Typography
-                            variant="subtitle1"
-                            fontSize="2.5rem"
-                            marginTop={"8rem"}
-                            marginBottom={"2rem"}
-                        >
-                            پروفسور سمیعی
-                        </Typography>
-                        <Typography lineHeight={"3.5rem"}>
-                            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                            از طراحان گرافیک است. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از
-                            صنعت چاپ و با استفاده از طراحان گرافیک لورم ایپسوم متن ساختگی با تولید
-                            سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
-                        </Typography>
-                    </Box>
-                    <Box flexBasis={"60%"} textAlign={"right"}>
-                        <img
-                            src={`${DrSamiee}`}
-                            alt="dr samieee"
-                            width={"273rem"}
-                            height={"271rem"}
-                        />
-                    </Box>
-                </Box>
-            </Box>
-
-            <Box
-                display={"flex"}
-                flexWrap={"wrap"}
-                gap={"5rem"}
-                margin={"7.5rem auto"}
-                className={`${classes.educationDetailsBox} ${"educationDetailsBox"}`}
-            >
-                {text.map((value: string, index: number) => {
-                    return (
-                        <ButtonKit disabled={disable} onClick={() => navigate(path[index])}>
-                            <Box
-                                width={"100%"}
-                                height={"16rem"}
-                                display={"flex"}
-                                borderRadius={"2rem"}
-                                flexDirection={"column"}
-                                alignItems={"center"}
-                                justifyContent={"center"}
-                                bgcolor={theme?.palette?.others.info.light}
-                            >
-                                <Box
-                                    display={"flex"}
-                                    justifyContent={"center"}
-                                    alignItems={"center"}
-                                    width={"5rem"}
-                                    height={"5rem"}
-                                    margin={"1rem"}
-                                    bgcolor={theme?.palette?.common.white}
-                                    borderRadius={"1rem"}
-                                    padding={"1rem"}
-                                >
-                                    {logo[index]}
-                                </Box>
-                                <Box>
-                                    <Typography variant="subtitle1">{value}</Typography>
-                                </Box>
-                            </Box>
-                        </ButtonKit>
-                    );
-                })}
-            </Box>
+              setBook(newValue);
+              setSelectValue(newValue);
+            }}
+          ></SelectKit>
         </Box>
-    );
+
+        <Box
+          className={`drSamieeBox`}
+          display={"flex"}
+          justifyContent={"space-around"}
+          alignItems={"center"}
+        >
+          <Box flexBasis={"40%"}>
+            <Typography
+              variant="subtitle1"
+              fontSize="2.5rem"
+              marginTop={"8rem"}
+              marginBottom={"2rem"}
+            >
+              پروفسور سمیعی
+            </Typography>
+            <Typography lineHeight={"3.5rem"}>
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+              گرافیک است. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از
+              طراحان گرافیک لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+              از طراحان گرافیک است.
+            </Typography>
+          </Box>
+          <Box flexBasis={"60%"} textAlign={"right"}>
+            <img src={`${DrSamiee}`} alt="dr samieee" width={"273rem"} height={"271rem"} />
+          </Box>
+        </Box>
+      </Box>
+
+      <Box
+        display={"flex"}
+        flexWrap={"wrap"}
+        gap={"5rem"}
+        margin={"7.5rem auto"}
+        className={`${classes.educationDetailsBox} ${"educationDetailsBox"}`}
+      >
+        {text.map((value: string, index: number) => {
+          return (
+            <ButtonKit disabled={disable} onClick={() => navigate(path[index])}>
+              <Box
+                width={"100%"}
+                height={"16rem"}
+                display={"flex"}
+                borderRadius={"2rem"}
+                flexDirection={"column"}
+                alignItems={"center"}
+                justifyContent={"center"}
+                bgcolor={theme?.palette?.others.info.light}
+              >
+                <Box
+                  display={"flex"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  width={"5rem"}
+                  height={"5rem"}
+                  margin={"1rem"}
+                  bgcolor={theme?.palette?.common.white}
+                  borderRadius={"1rem"}
+                  padding={"1rem"}
+                >
+                  {logo[index]}
+                </Box>
+                <Box>
+                  <Typography variant="subtitle1">{value}</Typography>
+                </Box>
+              </Box>
+            </ButtonKit>
+          );
+        })}
+      </Box>
+    </Box>
+  );
 };
 
 export default MajorRequirements;
